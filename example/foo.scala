a8543783c8315b09dc68465e1804c0d019819af3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-02-25 15:14:21 +0100

    WIP

--- example/foo.scala
+++ example/foo.scala
@@ -1,6 +1,8 @@
 case class A(a: Int)
 case class B(a: Int)
 object Main {
+  new A(a = 42)
   A.apply(a = 42).copy(a = 41).productElement(0)
+  new B(a = 42)
   B.apply(a = 42).copy(a = 41).productElement(0)
 }
