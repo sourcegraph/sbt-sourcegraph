ff79c7a5d69b448674b702ca3bf6f244a4e95ef9
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-05-23 16:10:58 +0200

    WIP

---
+++ base/Base.java
@@ -0,0 +1,3 @@
+package com.base;
+
+public class Base {}
---
+++ hello/Hello.java
@@ -0,0 +1,9 @@
+package com.hello;
+
+import com.base.Base;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println(new Base().toString());
+	}
+}
